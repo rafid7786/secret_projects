290110a8f29807df2c2ddba8e7302b09d2da7509
rafid7786 <rafidwafif@gmail.com>
2025-10-29 20:09:18 +0700

    Refactor code structure for improved readability and maintainability

--- main.js
+++ main.js
@@ -137,7 +137,7 @@ document.querySelector('#app').innerHTML = `
   </div>
 
   <audio id="bgMusic" loop>
-    <source src="./src/lagu1.mp3" type="audio/mpeg">
+    <source src="/lagu-1.mp3" type="audio/mpeg">
   </audio>
 `;
 
